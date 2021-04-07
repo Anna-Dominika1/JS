58f57224544d127c91d4512a60080758a36a59e3
unknown <you@example.com>
2021-04-07 11:26:47 +0300

    module1Finaltask5

--- 1_module/Final-task/js/task5.js
+++ 1_module/Final-task/js/task5.js
@@ -1,34 +1,32 @@
-// let prices;
-// const country = 'Китай';
+let prices;
+const country = prompt('Введіть країну в яку хочете щось надіслати!');
 
-// switch (country) {
-//   case 'Китай':
-//     prices = 100;
-//     console.log('Доставка в Китай буде коштувати 100 кредитів.')
-//     break;
+switch (country.toLowerCase()) {
+  case 'китай':
+    prices = 100;
+    console.log('Доставка в Китай буде коштувати 100 кредитів.')
+    break;
 
-//   case 'Чилі':
-//     prices = 250;
-//     console.log('Доставка в Чилі буде коштувати 250 кредитів.')
-//     break;
+  case 'чилі':
+    prices = 250;
+    console.log('Доставка в Чилі буде коштувати 250 кредитів.')
+    break;
 
-//   case 'Австралія':
-//     prices = 170;
-//     console.log('Доставка в Астралію буде коштувати 170 кредитів.')
-//     break;
+  case 'австралія':
+    prices = 170;
+    console.log('Доставка в Астралію буде коштувати 170 кредитів.')
+    break;
    
-//     case 'Індія':
-//       prices = 80;
-//       console.log('Доставка в Індію буде коштувати 80 кредитів.')
-//       break;
+    case 'індія':
+      prices = 80;
+      console.log('Доставка в Індію буде коштувати 80 кредитів.')
+      break;
 
-//       case 'Ямайка':
-//       prices = 120;
-//       console.log('Доставка в Ямайку буде коштувати 120 кредитів.')
-//       break;
+      case 'ямайка':
+      prices = 120;
+      console.log('Доставка в Ямайку буде коштувати 120 кредитів.')
+      break;
 
-//   default:
-//     console.log('У вашій країні доставка недоступна');
-// }
-
-// console.log(prices); 
+  default:
+    console.log('У вашій країні доставка недоступна');
+}
