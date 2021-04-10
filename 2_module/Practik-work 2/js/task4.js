6a771535c7df0c2f3882c222a7a37359a1babec0
unknown <you@example.com>
2021-04-10 13:25:17 +0300

    hwModule2PractikWork2

--- 2_module/Practik-work 2/js/task4.js
+++ 2_module/Practik-work 2/js/task4.js
@@ -1,10 +1,9 @@
-// const myNewArrowFunction = function() {
 
-//     const  myNewArrowFunction1 = [1, 2, 3];
-//     const  myNewArrowFunction2 = [100, 200, 300, 400, 500];
-//     const  myNewArrowFunction3 = ["Js", "Python", "Java", "PHP", "C++"];
-//   };
+const myNewArrowFunction = function (...myNewArrowFunction) {
 
-//   myNewArrowFunction()
-
-//   console.log(myNewArrowFunction)
+    console.log(myNewArrowFunction);
+  }
+  
+  myNewArrowFunction(1, 2, 3);
+  myNewArrowFunction(100, 200, 300, 400, 500);
+  myNewArrowFunction("Js", "Python", "Java", "PHP", "C++");
--- 2_module/Practik-work 2/js/task5.js
+++ 2_module/Practik-work 2/js/task5.js
@@ -1,42 +1,60 @@
+//  const myAverageScore = function(array){
+//      let total = 0;
+//      for(const item of array){
+//        total += item
+//      }
 
-
-// const myAverageScore = function(number)  {
-    
-    
-//     if (number === 0) {
-//       console.log('Для проведення операції введіть суму більше нуля.');
-//       return;
+//      const averaige = total / array.length
+//     //  console.log(averaige)
+//     if (averaige>=91 && averaige<=100){
+//       return 'Myaveragescore: A'
 //     }
-  
-//    else if  (91 <= 100) {
-//       console.log('Myaveragescore: A');
-//       return;
+//     if (averaige>=81 && averaige<=90){
+//       return 'Myaveragescore: B'
+//     }
+//     if (averaige>=71 && averaige<=80){
+//       return 'Myaveragescore: C'
+//     }
+//     if (averaige<=70){
+//       return 'Myaveragescore: D'
 //     }
     
-//     else if  (81 <= 90) {
-//         console.log('Myaveragescore: B');
-//         return;
-//       }
+//  };
 
-//       else if  (81 <= 90) {
-//         console.log('Myaveragescore: C');
-//         return;
-//       }
-     
-//       else if  (71 <= 80) {
-//         console.log('Myaveragescore: D');
-//         return;
-//       }
-      
-//       else if  (70 >= 0 ) {
-//         console.log('Myaveragescore');
-//         return;
-//       }
+
+
+//  console.log(myAverageScore([100, 75, 81, 96]));
+//  console.log(myAverageScore([45, 63, 85, 70]));
+//  console.log(myAverageScore([77, 82, 60, 58]));
+//  console.log(myAverageScore([93, 99, 93, 96]));
   
-//   };
-  
-//   console.log(myAverageScore([100, 75, 81, 96]));
-//   console.log(myAverageScore([45, 63, 85, 70]));
-//   console.log(myAverageScore([77, 82, 60, 58]));
-//   console.log(myAverageScore([93, 99, 93, 96]));
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
