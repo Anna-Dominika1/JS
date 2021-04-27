73da32e797d348c24519220891350db77df2bd3d
unknown <you@example.com>
2021-04-27 17:12:55 +0300

    HW4MODULE

--- 4_module/Final-task/js/task2.js
+++ 4_module/Final-task/js/task2.js
@@ -2,8 +2,7 @@ const inventory = {
   items: ['Knife', 'Gas mask'],
   add(itemName) {
     console.log(`Adding ${itemName} to inventory`);
-
-    this.items(itemName.push());
+    this.items.push(itemName);
   },
   remove(itemName) {
     console.log(`Removing ${itemName} from inventory`);
@@ -14,18 +13,16 @@ const inventory = {
 
 const invokeInventoryAction = function (itemName, action) {
   console.log(`Invoking action on ${itemName}`);
-  
+  action.itemName
 };
-invokeInventoryAction()
 
-
-invokeInventoryAction('Medkit', inventory.add);
+invokeInventoryAction.bind('Medkit', inventory.add);
 // Invoking action on Medkit
 // Adding Medkit to inventory
 
 console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
 
-invokeInventoryAction('Gas mask', inventory.remove);
+invokeInventoryAction.bind('Gas mask', inventory.remove);
 // Invoking action on Gas mask
 // Removing Gas mask from inventory
 
