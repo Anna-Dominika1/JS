79042a347beb17a78d75c0d07ef84d9bc30236b7
unknown <you@example.com>
2021-07-06 17:11:52 +0300

    HONEWORK5

---
+++ 5_module/js/task3.js
@@ -0,0 +1,29 @@
+// class Storage {
+//     constructor(items) {
+//         this.items = items;
+//     }
+//     getItems() {
+//         return this.items;
+//     }
+//     addItem(item) {
+//         this.items.push(item);
+//     }
+//     removeItem(item) {
+//         this.items.splice(this.items.indexOf(item), 1);
+//     }
+// }
+// const storage = new Storage([
+//     'Нанітоіди',
+//     'Пролонгер',
+//     'Залізні жупи',
+//     'Антигравітатор',
+// ]);
+
+// const items = storage.getItems();
+// console.log(items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор" ]
+
+// storage.addItem('Дроїд');
+// console.log(storage.items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор", "Дроїд" ]
+
+// storage.removeItem('Пролонгер');
+// console.log(storage.items); // [ "Нанітоіди", "Залізні жупи", "Антигравітатор", "Дроїд" ]
---
+++ 5_module/js/task4.js
@@ -0,0 +1,32 @@
+// class StringBuilder {
+//     constructor(value) {
+//         this._value = value
+//     }
+//     get value() {
+//         return this._value;
+//     }
+
+//     append(a) {
+//         return this._value = this._value + a
+//     }
+
+//     prepend(b) {
+//         return this._value = b + this._value;
+//     }
+
+//     pad(c) {
+//         return this._value = c + this._value + c;
+//     }
+// }
+
+// const builder = new StringBuilder('.');
+// console.log(builder.append('^'));// '.^'
+
+// builder.prepend('^');
+// console.log(builder.value); // '^.^'
+
+// builder.pad('=');
+// console.log(builder.value); // '=^.^='
+
+
+
